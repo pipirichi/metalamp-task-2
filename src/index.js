a130c7287989c74549c24f1c76833dcafc2f6f2b
pipirichi <89348762+pipirichi@users.noreply.github.com>
2022-04-05 19:00:04 +0300

    Сверстала date-dropdown.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./index.pug";
 import "./style.scss";
 import "./elements/masked-text-field/masked-text-field";
+import "./elements/date-dropdown/date-dropdown";
 
 console.log("Как интересно!")
