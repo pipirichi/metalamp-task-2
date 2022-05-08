1772e0dbd52f5345af69e20ff3a15b779c2dca6f
pipirichi <89348762+pipirichi@users.noreply.github.com>
2022-05-08 20:57:39 +0300

    Проведен небольшой рефакторинг

--- src/elements/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/elements/expandable-checkbox-list/expandable-checkbox-list.js
@@ -3,9 +3,7 @@ import $ from "jquery";
 const slideToggle = $(".added-conveniences__toggle");
 const conveniencesList = $(".conveniences-list");
 
-slideToggle.on("click", function(){ 
+slideToggle.on("click", function () {
   conveniencesList.slideToggle();
-  slideToggle.text(slideToggle.text()== "expand_more" ? "expand_less" : "expand_more");
+  slideToggle.text(slideToggle.text() == "expand_more" ? "expand_less" : "expand_more");
 });
-// изменить название кнопки?
-// ааааааааа все поменять.............
--- src/elements/like-button/like-button.js
+++ src/elements/like-button/like-button.js
@@ -1,11 +1,11 @@
-import $ from "jquery"; 
+import $ from "jquery";
 
 const likeButton = $(".like-button");
 const likeHeart = $(".like-button__heart");
 const likeCount = $(".like-button__count");
 
-likeButton.on("click", function(){ 
+likeButton.on("click", function () {
   $(this).toggleClass("like-button_active");
-  likeHeart.text(likeHeart.text()== "favorite_border" ? "favorite" : "favorite_border");
+  likeHeart.text(likeHeart.text() == "favorite_border" ? "favorite" : "favorite_border");
   likeCount.text($(this).hasClass("like-button_active") ? +likeCount.text() + 1 : +likeCount.text() - 1);
 });
